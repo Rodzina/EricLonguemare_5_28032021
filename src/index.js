e9c7c573e4153ff32d01cc6e8a7c6daf4f6ced1f
el-opcr <el.openclassrooms@gmail.com>
2021-04-10 04:15:25 +0200

    conf Boostrap V5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import * as bootstrap from 'bootstrap';
+// Supprimer les modules non utilisés. Si Popover, Tooltip, Dropdown sont utilisés : popper.js sera ajouté.
+//import {Alert, Button, Carousel, Collapse, Dropdown, Modal, Popover, ScrollSpy, Tab, Toast, Tooltip} from "bootstrap";
+// utilise ESM
+//styles personnalisés
+import './assets/scss/custom.scss'; // Import our scss file
+
 const entryPoint = "https://polar-retreat-13131.herokuapp.com/api/teddies/"
 
 let bears = [];
@@ -43,5 +50,17 @@ function displayBears(myBear) {
     myCard.appendChild(myCardTitle);
     const myCardTitleText = document.createTextNode(myBear.name);
     myCardTitle.appendChild(myCardTitleText);
+}
+
+//Check boostrap availability
+
+if (typeof bootstrap !== 'undefined' || bootstrap !== null) {
+    console.log("Alert version : " + bootstrap.Alert.VERSION);
+    console.log("Button version : " + bootstrap.Button.VERSION);
+    console.log("Popover version : " + bootstrap.Popover.VERSION);
+    console.log("Tooltip version : " + bootstrap.Tooltip.VERSION);
+    window.bootstrap = bootstrap;
+} else {
+    console.log("index.js : Bootstrap non défini");
 }
 
