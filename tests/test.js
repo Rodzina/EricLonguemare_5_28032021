82ba104e66bde2ea132ea715b0aa7ba503ba65e0
el-opcr <el.openclassrooms@gmail.com>
2021-05-29 22:20:29 +0200

    WIP : add some product to cart

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 const puppeteer = require('puppeteer')
 
+// https://stackoverflow.com/questions/51789038/set-localstorage-items-before-page-loads-in-puppeteer
+
 const myUrls = [
   ['https://el-opcr.github.io/EricLonguemare_5_28032021/dist/', 'tests/index.html'],
   ['https://el-opcr.github.io/EricLonguemare_5_28032021/dist/teddy.html?id=5beaa8bf1c9d440000a57d94', 'tests/teddie.html'],
   ['https://el-opcr.github.io/EricLonguemare_5_28032021/dist/panier.html?panier', 'tests/cart.html']
 ]
+
+const theCart = []
 
 const getPage = async myUrls => {
   // scrape Pages for W3C validation
