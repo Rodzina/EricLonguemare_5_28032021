8865ff4a0dd585f829080a9e550026253893f34c
el-opcr <el.openclassrooms@gmail.com>
2021-05-29 17:57:21 +0200

    Tests : scrape index page rendered HTML

---
+++ tests/js/test.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const url = process.argv[2] || 'https://el-opcr.github.io/EricLonguemare_5_28032021/dist/';
+const path = process.argv[3] || `index.html`;
+
+(async function main() {
+  const browser = await puppeteer.launch();
+  const [page] = await browser.pages();
+
+  await page.goto(url, { waitUntil: 'networkidle0' });
+  fs.writeFileSync(path, await page.content());
+
+  await browser.close();
+})().catch(console.error);
